9760862045fcf5131e26e05751a061d069536de1
Kate Whalen <whalenka88@gmail.com>
2017-11-03 14:26:01 +0000

    Add auth checks, resolve lib dependency incompatibility, add login error page

--- hq/app/controllers/HQController.scala
+++ hq/app/controllers/HQController.scala
@@ -1,24 +1,29 @@
 package controllers
 
+import auth.SecurityHQAuthActions
 import aws.AWS
 import aws.support.{TrustedAdvisor, TrustedAdvisorExposedIAMKeys}
 import config.Config
 import model.ExposedIAMKeyDetail
 import play.api._
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 import utils.attempt.PlayIntegration.attempt
 
 import scala.concurrent.ExecutionContext
 
 
-class HQController(val config: Configuration)(implicit val ec: ExecutionContext) extends Controller {
+class HQController(val config: Configuration)
+                  (implicit val ec: ExecutionContext, val wsClient: WSClient)
+  extends Controller  with SecurityHQAuthActions {
+
   private val accounts = Config.getAwsAccounts(config)
 
-  def index = Action {
+  def index = AuthAction {
     Ok(views.html.index(accounts))
   }
 
-  def account(accountId: String) = Action.async {
+  def account(accountId: String) = AuthAction.async {
     attempt {
       for {
         account <- AWS.lookupAccount(accountId, accounts)
@@ -28,11 +33,11 @@ class HQController(val config: Configuration)(implicit val ec: ExecutionContext)
     }
   }
 
-  def iam = Action {
+  def iam = AuthAction {
     Ok(views.html.iam.iam())
   }
 
-  def iamAccount(accountId: String) = Action.async {
+  def iamAccount(accountId: String) = AuthAction.async {
     attempt {
       for {
         account <- AWS.lookupAccount(accountId, accounts)
@@ -46,7 +51,7 @@ class HQController(val config: Configuration)(implicit val ec: ExecutionContext)
     }
   }
 
-  def dependencies = Action {
+  def dependencies = AuthAction {
     Ok(views.html.dependencies.dependencies())
   }
 }
