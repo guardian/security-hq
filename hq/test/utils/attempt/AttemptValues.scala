51899451ef733909d6ca1f5f19b634d32839152e
Adam Fisher <adam.fisher@guardian.co.uk>
2017-11-07 15:37:49 +0000

    Use "clue" to provide test feedback on failed attempts

--- hq/test/utils/attempt/AttemptValues.scala
+++ hq/test/utils/attempt/AttemptValues.scala
@@ -1,20 +1,27 @@
 package utils.attempt
 
+import org.scalatest.Matchers
 import org.scalatest.exceptions.TestFailedException
 
 import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
 
 
-trait AttemptValues {
+trait AttemptValues extends Matchers {
   implicit class RichAttempt[A](attempt: Attempt[A]) {
     def value()(implicit ec: ExecutionContext): A = {
-      Await.result(attempt.asFuture, 5.seconds).fold[A] (
-        { fa =>
-          throw new TestFailedException(fa.logString, 10)
-        },
-        identity
-      )
+      val result = Await.result(attempt.asFuture, 5.seconds)
+      withClue {
+        result.fold(
+          fa => s"${fa.logString} -",
+          _ => ""
+        )
+      } {
+        result.fold[A](
+          _ => throw new TestFailedException("Could not extract value from failed Attempt", 10),
+          identity
+        )
+      }
     }
 
     def isFailedAttempt()(implicit ec: ExecutionContext): Boolean = {
