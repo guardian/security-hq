ecc0a733d8d62084fc970eb6d7ceba480acccdc5
Kate Whalen <whalenka88@gmail.com>
2017-11-24 14:43:57 +0000

    Fixes for types, sass-lint, and indentation

--- hq/test/logic/SecurityGroupDisplayTest.scala
+++ hq/test/logic/SecurityGroupDisplayTest.scala
@@ -8,12 +8,12 @@ import SecurityGroupDisplay._
 class SecurityGroupDisplayTest extends FreeSpec with Matchers {
   "resourceIcons" - {
     "returns 2 when there are two instances" in {
-      val usages = Set[SGInUse](Instance("instance-1"), Instance("instance-2"))
+      val usages = List[SGInUse](Instance("instance-1"), Instance("instance-2"))
       resourceIcons(usages).instances shouldEqual 2
     }
 
     "returns individual counts for the different resource types" in {
-      val usages = Set[SGInUse](Instance("instance"), ELB("elb-1"), ELB("elb-2"), UnknownUsage("unknown", ""))
+      val usages = List[SGInUse](Instance("instance"), ELB("elb-1"), ELB("elb-2"), UnknownUsage("unknown", ""))
       resourceIcons(usages).instances shouldEqual 1
       resourceIcons(usages).elbs shouldEqual 2
       resourceIcons(usages).unknown shouldEqual 1
