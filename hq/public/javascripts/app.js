b63bf88782f74253f17b962764a6cdc2bb2112e8
Kate Whalen <whalenka88@gmail.com>
2017-12-04 11:07:42 +0000

    Run Prettier against the JS and CSS

--- hq/public/javascripts/app.js
+++ hq/public/javascripts/app.js
@@ -1,21 +1,25 @@
-jQuery(function($){
-    // idempotent redirect to HTTPS
-    if (! /^https/.test(window.location.protocol)) {
-        console.log("insecure");
-        // window.location.replace(location.href.replace(/^http:/, "https:"))
-    }
+jQuery(function($) {
+  // idempotent redirect to HTTPS
+  if (!/^https/.test(window.location.protocol)) {
+    console.log('insecure');
+    // window.location.replace(location.href.replace(/^http:/, "https:"))
+  }
 
-    $('.modal').modal();
+  $('.modal').modal();
 });
 
 // extensions to the security groups page
 $(document).ready(() => {
-    $('.js-sg-details').hover(
-        function() {
-            $(this).find('.collapsible-header').click();
-        },
-        function() {
-            $(this).find('.collapsible-header').click();
-        }
-    );
+  $('.js-sg-details').hover(
+    function() {
+      $(this)
+        .find('.collapsible-header')
+        .click();
+    },
+    function() {
+      $(this)
+        .find('.collapsible-header')
+        .click();
+    }
+  );
 });
