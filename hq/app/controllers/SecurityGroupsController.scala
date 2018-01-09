4da1c34eddfcb22bfeb2835ff5ea6b917dfd6984
Kate Whalen <whalenka88@gmail.com>
2018-01-09 17:53:13 +0000

    Add a cache service and integrate it with Security Groups

--- hq/app/controllers/SecurityGroupsController.scala
+++ hq/app/controllers/SecurityGroupsController.scala
@@ -1,7 +1,5 @@
 package controllers
 
-import java.util.concurrent.Executors
-
 import auth.SecurityHQAuthActions
 import aws.AWS
 import aws.ec2.EC2
@@ -9,34 +7,31 @@ import config.Config
 import play.api._
 import play.api.libs.ws.WSClient
 import play.api.mvc._
+import services.CacheService
+import utils.attempt.Attempt
 import utils.attempt.PlayIntegration.attempt
 
 import scala.concurrent.ExecutionContext
 
 
-class SecurityGroupsController(val config: Configuration)
+class SecurityGroupsController(val config: Configuration, cacheService: CacheService)
                               (implicit val ec: ExecutionContext, val wsClient: WSClient, val bodyParser: BodyParser[AnyContent], val controllerComponents: ControllerComponents, val assetsFinder: AssetsFinder)
   extends BaseController with SecurityHQAuthActions {
 
   private val accounts = Config.getAwsAccounts(config)
 
-  // highly parallel for making simultaneous requests across all AWS accounts
-  val highlyAsyncExecutionContext = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+  def securityGroups = authAction {
+    val allFlaggedSgs = cacheService.getAllSgs()
+    val sortedFlaggedSgs = EC2.sortAccountByFlaggedSgs(allFlaggedSgs.toList)
 
-  def securityGroups = authAction.async {
-    attempt {
-      for {
-        allFlaggedSgs <- EC2.allFlaggedSgs(accounts)(highlyAsyncExecutionContext)
-        sortedFlaggedSgs = EC2.sortAccountByFlaggedSgs(allFlaggedSgs)
-      } yield Ok(views.html.sgs.sgs(sortedFlaggedSgs))
-    }
+    Ok(views.html.sgs.sgs(sortedFlaggedSgs))
   }
 
   def securityGroupsAccount(accountId: String) = authAction.async {
     attempt {
       for {
         account <- AWS.lookupAccount(accountId, accounts)
-        flaggedSgs <- EC2.flaggedSgsForAccount(account)
+        flaggedSgs <- Attempt.fromEither(cacheService.getSgsForAccount(account))
       } yield Ok(views.html.sgs.sgsAccount(account, flaggedSgs))
     }
   }
