448375fba00ed95120c04f524907c7e4c2cb8d5b
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-01-25 13:10:02 +0000

    Simplify Serializers code

--- hq/app/model/Serializers.scala
+++ hq/app/model/Serializers.scala
@@ -7,35 +7,17 @@ object Serializers {
 
   implicit val snykErrorFormat: Reads[SnykError] = Json.reads[SnykError]
 
-  implicit val snykOrgFormat: Format[SnykOrg] = (
-    (JsPath \ "name").format[String]
-      and
-      (JsPath \ "id").format[String]
-    )(SnykOrg.apply, unlift(SnykOrg.unapply))
+  implicit val snykOrgFormat: Format[SnykOrg] = Json.format[SnykOrg]
 
-  implicit val snykProjectFormat: Format[SnykProject] = (
-    (JsPath \ "name").format[String]
-      and
-      (JsPath \ "id").format[String]
-    )(SnykProject.apply, unlift(SnykProject.unapply))
+  implicit val snykProjectFormat: Format[SnykProject] = Json.format[SnykProject]
 
-  implicit val snykIssueReads: Reads[SnykIssue] = (
-    (JsPath \ "title").read[String]
-      and
-      (JsPath \ "id").read[String]
-      and
-      (JsPath \ "severity").read[String]
-    )(SnykIssue.apply _)
+  implicit val snykIssueReads: Format[SnykIssue] = Json.format[SnykIssue]
 
   implicit val snykProjectIssuesReads: Reads[SnykProjectIssues] = (
     Reads.pure("Unknown")
-      and
-      Reads.pure("Unknown")
-      and
-      (JsPath \ "ok").read[Boolean]
-      and
-      (JsPath \ "issues" \ "vulnerabilities").read[List[SnykIssue]]
+    and Reads.pure("Unknown")
+    and (JsPath \ "ok").read[Boolean]
+    and (JsPath \ "issues" \ "vulnerabilities").read[List[SnykIssue]]
     )(SnykProjectIssues.apply _)
-
 
 }
