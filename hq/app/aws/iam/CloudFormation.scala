b5ca1bd6a6c5aa107be64ff640e67d6e1f6ec147
Kate Whalen <whalenka88@gmail.com>
2018-02-15 13:49:13 +0000

    Discover regions, then get all stacks in all accessible regions

--- hq/app/aws/iam/CloudFormation.scala
+++ hq/app/aws/iam/CloudFormation.scala
@@ -2,9 +2,12 @@ package aws.iam
 
 import aws.AWS
 import aws.AwsAsyncHandler.{awsToScala, handleAWSErrs}
+import com.amazonaws.auth.AWSCredentialsProviderChain
 import com.amazonaws.regions.{Region, Regions}
-import com.amazonaws.services.cloudformation.{AmazonCloudFormationAsync, AmazonCloudFormationAsyncClient}
+import com.amazonaws.services.cloudformation.{AmazonCloudFormationAsync, AmazonCloudFormationAsyncClientBuilder}
 import com.amazonaws.services.cloudformation.model.{DescribeStacksRequest, DescribeStacksResult, Stack}
+import com.amazonaws.services.ec2.model.{DescribeRegionsRequest, DescribeRegionsResult}
+import com.amazonaws.services.ec2.{AmazonEC2Async, AmazonEC2AsyncClientBuilder}
 import model.AwsAccount
 import utils.attempt.Attempt
 
@@ -12,11 +15,28 @@ import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
 
 object CloudFormation {
-  private[iam] def client(account: AwsAccount, region: Region = Region.getRegion(Regions.EU_WEST_1)): AmazonCloudFormationAsync = {
-    val auth = AWS.credentialsProvider(account)
-    AmazonCloudFormationAsyncClient.asyncBuilder()
+  private def cloudClient(auth: AWSCredentialsProviderChain, region: Region = Region.getRegion(Regions.EU_WEST_1)): AmazonCloudFormationAsync = {
+    AmazonCloudFormationAsyncClientBuilder.standard()
       .withCredentials(auth)
-      .withRegion(Option(Regions.getCurrentRegion).getOrElse(region).getName).build()
+      .withRegion(region.getName)
+      .build()
+  }
+  private[iam] def client(awsAccount: AwsAccount, region: Region): AmazonCloudFormationAsync = {
+    val auth = AWS.credentialsProvider(awsAccount)
+    cloudClient(auth, region)
+  }
+
+  def describeRegionsClient(account: AwsAccount, region: Region = Region.getRegion(Regions.EU_WEST_1)): AmazonEC2Async = {
+    val auth = AWS.credentialsProvider(account)
+    AmazonEC2AsyncClientBuilder.standard()
+      .withCredentials(auth)
+      .withRegion(Option(Regions.getCurrentRegion).getOrElse(region).getName)
+      .build()
+  }
+
+  def describeRegions(client: AmazonEC2Async)(implicit ec: ExecutionContext): Attempt[DescribeRegionsResult] = {
+    val request = new DescribeRegionsRequest()
+    handleAWSErrs(awsToScala(client.describeRegionsAsync)(request))
   }
 
   private[iam] def getStackDescriptions(client: AmazonCloudFormationAsync)(implicit ec: ExecutionContext): Attempt[DescribeStacksResult] = {
@@ -24,9 +44,20 @@ object CloudFormation {
     handleAWSErrs(awsToScala(client.describeStacksAsync)(request))
   }
 
-  private[iam] def getUserStacks(stacks: DescribeStacksResult): List[Stack] = {
+  private[iam] def getStacksFromAllRegions(account: AwsAccount)(implicit ec: ExecutionContext): Attempt[List[DescribeStacksResult]] = {
+    val regionClient = describeRegionsClient(account)
     for {
-      stack <- stacks.getStacks.asScala.toList
+      describeRegionsResult <- describeRegions(regionClient)
+      regions = describeRegionsResult.getRegions.asScala.toList
+      clients = regions.map(region => CloudFormation.client(account, Region.getRegion(Regions.fromName(region.getRegionName))))
+      describeStacksResult <- Attempt.traverse(clients)(getStackDescriptions)
+    } yield describeStacksResult
+  }
+
+  private[iam] def getUserStacks(stacksResults: List[DescribeStacksResult]): List[Stack] = {
+    for {
+      result <- stacksResults
+      stack <- result.getStacks.asScala.toList
       output <- stack.getOutputs.asScala.toList
       if output.getOutputValue.contains(":user")
     } yield stack
