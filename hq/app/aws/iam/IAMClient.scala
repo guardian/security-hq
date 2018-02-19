41490a39d4728ef05a984194716e493a13712025
Kate Whalen <whalenka88@gmail.com>
2018-02-19 12:34:47 +0000

    Add Stacks to the model and parse the response from AWS

--- hq/app/aws/iam/IAMClient.scala
+++ hq/app/aws/iam/IAMClient.scala
@@ -39,8 +39,7 @@ object IAMClient {
       _ <- Retry.until(generateCredentialsReport(client), CredentialsReport.isComplete, "Failed to generate credentials report", delay)
       report <- getCredentialsReport(client)
       stacks <- CloudFormation.getStacksFromAllRegions(account)
-      userStacks = CloudFormation.extractUserStacks(stacks)
-      enrichedReport = CredentialsReport.enrichReportWithStackDetails(report, userStacks)
+      enrichedReport = CredentialsReport.enrichReportWithStackDetails(report, stacks)
     } yield ReportDisplay.toCredentialReportDisplay(enrichedReport)
   }
 
