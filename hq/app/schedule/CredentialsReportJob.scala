09d89bb4a51167e983f9ef76781335083e288c12
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-04-12 17:29:13 +0100

    refactors credentials notification function so it's testable

--- hq/app/schedule/CredentialsReportJob.scala
+++ hq/app/schedule/CredentialsReportJob.scala
@@ -1,4 +1,4 @@
-import com.gu.anghammarad.models.{AwsAccount => Account}
+import com.gu.anghammarad.models.{Notification, AwsAccount => Account}
 import logic.DateUtils
 import model.{AccessKey, AwsAccount, CredentialReportDisplay, CronSchedule}
 import play.api.Logging
@@ -21,22 +21,30 @@ class CredentialsReportJob(enabled: Boolean, cacheService: CacheService)(executi
     } else {
       logger.info(s"Running scheduled job: $description")
 
-      getCredentialsReport().foreach{ case (awsAccount, result) =>
-        result.foreach{ credsReport =>
+      val someValue: List[Either[FailedAttempt, Notification]] = getCredentialsReport().toList.map{ case (awsAccount, eFCreds) =>
+        eFCreds.map{ credsReport =>
           val outdatedKeys = findOldAccessKeys(credsReport)
           val missingMfa = findMissingMfa(credsReport)
           val message = createMessage(outdatedKeys, missingMfa)
-          val email = createNotification(Account(awsAccount.id), message)
-          send(email, topicArn, ???)(executionContext)
-          logger.info(s"Completed scheduled job: $description")
+          createNotification(Account(awsAccount.id), message)
         }
       }
+      someValue.foreach{ result: Either[FailedAttempt, Notification] =>
+        result match {
+          case Left(value) =>
+            logger.info(s"check this issue out security devs! $value")
+          case Right(email) =>
+            send(email, topicArn, ???)(executionContext)
+            logger.info(s"Completed scheduled job: $description")
+        }
+       }
     }
   }
 
   def getCredentialsReport(): Map[AwsAccount, Either[FailedAttempt, CredentialReportDisplay]] = cacheService.getAllCredentials
 
   def findOldAccessKeys(credsReport: CredentialReportDisplay): CredentialReportDisplay = {
+    //TODO write tests:Return true if any key is older than 90 days
     def hasOutdatedKey(keys: List[AccessKey]): Boolean = keys.exists(key => DateUtils.dayDiff(key.lastRotated).getOrElse(1L) > 90L) //TODO fix Long issue
     val filteredMachines = credsReport.machineUsers.filter(user => hasOutdatedKey(List(user.key1, user.key2)))
     val filteredHumans = credsReport.humanUsers.filter(user => hasOutdatedKey(List(user.key1, user.key2)))
