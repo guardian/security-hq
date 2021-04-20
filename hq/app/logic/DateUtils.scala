072f0fa1ac907dd036073aa9256d6a1092f489b8
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2021-04-20 15:48:28 +0100

    Add warning for stale data

--- hq/app/logic/DateUtils.scala
+++ hq/app/logic/DateUtils.scala
@@ -12,7 +12,9 @@ object DateUtils {
   }
   def toISOString(dateTime: DateTime) = ISODateTimeFormat.dateTime().print(dateTime)
 
-  def dayDiff(date: Option[DateTime]): Option[Long] =  date.map(new Duration(_, DateTime.now(DateTimeZone.UTC)).getStandardDays)
+  def dayDiff(date: Option[DateTime]): Option[Long] =  date.map(dayDiff)
+
+  def dayDiff(date: DateTime): Long =  new Duration(date, DateTime.now(DateTimeZone.UTC)).getStandardDays
 
   def printTime(date: DateTime): String = date.toString("HH:mm")
 }
