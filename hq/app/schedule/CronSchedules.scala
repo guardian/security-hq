a9c854f158574d3f66958e974f22248e8d460020
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-06-23 12:16:54 +0100

    Change IAM job CRON schedule to daily The IAM job sends notifications when a permanent credential either needs rotating or needs multi-factor authentication.
    Currently the job runs monthly, because previously it was just sending
    one email notification.
    
    Now, it sends up to three notifications over a period of three weeks,
    alerting the AWS account which holds this permanent credential that
    it will be disabled if unchanged. Here are the PRs: #256 and #252.
    
    Given this change, the job now needs to run daily in order to
    implement this new functionality.

--- hq/app/schedule/CronSchedules.scala
+++ hq/app/schedule/CronSchedules.scala
@@ -7,4 +7,5 @@ object CronSchedules {
   val firstMondayOfEveryMonth = CronSchedule("0 0 8 ? * 2#1", "At 8am, on the 1st Monday of the month, every month")
   val secondMondayOfEveryMonth = CronSchedule("0 0 8 ? * 2#2", "At 8am, on the 1st Monday of the month, every month")
   val everyThursday = CronSchedule("0 0 9 ? * THU", "At 8am, on the 1st Monday of the month, every month")
+  val everyWeekDay = CronSchedule("0 0 14 ? * MON-FRI", "Every week day at 2pm")
 }
