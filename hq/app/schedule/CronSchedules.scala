b23e1c1769da51590a2b72a43db066240a1a7b47
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-09-13 16:53:58 +0100

    trying again

--- hq/app/schedule/CronSchedules.scala
+++ hq/app/schedule/CronSchedules.scala
@@ -4,5 +4,5 @@ import model.CronSchedule
 
 //a helpful quartz cron generator: https://www.freeformatter.com/cron-expression-generator-quartz.html
 object CronSchedules {
-  val everyWeekDay = CronSchedule("0 0 15 ? * MON-FRI *", "At 3pm, every week day")
+  val everyWeekDay = CronSchedule("0 0 9 ? * MON-FRI *", "At 9am, every week day")
 }
