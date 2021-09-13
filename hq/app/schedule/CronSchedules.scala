06fc7a0a1876d4432cfbcfd6fb406a8af1214d89
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-09-13 15:21:03 +0100

    Turn the IAM job on (again)

--- hq/app/schedule/CronSchedules.scala
+++ hq/app/schedule/CronSchedules.scala
@@ -4,5 +4,5 @@ import model.CronSchedule
 
 //a helpful quartz cron generator: https://www.freeformatter.com/cron-expression-generator-quartz.html
 object CronSchedules {
-  val everyWeekDay = CronSchedule("0 0 14 ? * MON-FRI *", "At 2pm, every week day")
+  val everyWeekDay = CronSchedule("0 0 15 ? * MON-FRI *", "At 3pm, every week day")
 }
