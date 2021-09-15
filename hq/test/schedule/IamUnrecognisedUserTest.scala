b88984d53f76751f24eff219f4b92460e5eaf549
Jon Soul <jonathan.soul@guardian.co.uk>
2021-09-15 16:44:48 +0100

    Update tests

--- hq/test/schedule/IamUnrecognisedUserTest.scala
+++ hq/test/schedule/IamUnrecognisedUserTest.scala
@@ -1,11 +1,10 @@
 package schedule
 
 import com.gu.janus.model.{ACL, AwsAccount, JanusData, SupportACL}
-import model.{AccessKey, CredentialReportDisplay, Green, HumanUser, NoKey, Tag, AwsAccount => Account}
+import model._
 import org.joda.time.{DateTime, Seconds}
 import org.scalatest.{FreeSpec, Matchers}
-import schedule.unrecognised.IamUnrecognisedUsers.{filterUnrecognisedIamUsers, getHumanUsers, getJanusUsernames}
-import utils.attempt.FailedAttempt
+import schedule.unrecognised.IamUnrecognisedUsers.{filterUnrecognisedIamUsers, getJanusUsernames}
 
 class IamUnrecognisedUserTest extends FreeSpec with Matchers {
   val humanUser1 = HumanUser("", true, AccessKey(NoKey, None), AccessKey(NoKey, None), Green, None, None, List(Tag("name", "ade.bimbola")))
@@ -31,16 +30,11 @@ class IamUnrecognisedUserTest extends FreeSpec with Matchers {
 
       getJanusUsernames(dummyJanusData) shouldEqual List("firstName.secondName")
     }
-    "get human users from credentials report" in {
-      val credsReport: Map[Account, Either[FailedAttempt, CredentialReportDisplay]] =
-        Map(Account("id", "name", "roleArn", "accountNumber") -> Right(credsReportDisplay))
-      val result: List[HumanUser] = List(humanUser1, humanUser2, humanUser3, humanUser4)
-      getHumanUsers(credsReport) shouldEqual result
-    }
     "get unrecognised human users" in {
       val permanentIamUsers: List[HumanUser] = List(humanUser1, humanUser2, humanUser3, humanUser4)
+      val vulnerableUsers: List[VulnerableUser] = List(VulnerableUser.fromIamUser(humanUser4))
       val janusUsers: List[String] = List("ade.bimbola", "john.akindele", "khadija.omodara")
-      filterUnrecognisedIamUsers(permanentIamUsers, "name", janusUsers) shouldEqual List(humanUser4)
+      filterUnrecognisedIamUsers(permanentIamUsers, "name", janusUsers) shouldEqual vulnerableUsers
     }
   }
 }
