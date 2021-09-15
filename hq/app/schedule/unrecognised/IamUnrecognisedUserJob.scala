4eac888efcc07807b3e66bdfce81691e3e3df051
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-09-15 05:54:15 +0100

    Find IAM users who are not Janus users

--- hq/app/schedule/unrecognised/IamUnrecognisedUserJob.scala
+++ hq/app/schedule/unrecognised/IamUnrecognisedUserJob.scala
@@ -1,12 +1,15 @@
 package schedule.unrecognised
 
 import com.gu.janus.model.{ACL, AwsAccount, JanusData, SupportACL}
-import model.CronSchedule
+import model.{CredentialReportDisplay, CronSchedule, HumanUser, AwsAccount => Account}
 import org.joda.time.Seconds
 import play.api.Logging
+import schedule.unrecognised.IamUnrecognisedUsers.{filterUnrecognisedIamUsers, getHumanUsers, getJanusUsernames}
 import schedule.{CronSchedules, JobRunner}
+import services.CacheService
+import utils.attempt.FailedAttempt
 
-class IamUnrecognisedUserJob() extends JobRunner with Logging {
+class IamUnrecognisedUserJob(cacheService: CacheService) extends JobRunner with Logging {
   override val id: String = "unrecognised-iam-users"
   override val description: String = "Check for and remove unrecognised human IAM users"
   override val cronSchedule: CronSchedule = CronSchedules.everyWeekDay
@@ -27,7 +30,11 @@ class IamUnrecognisedUserJob() extends JobRunner with Logging {
     None
   )
 
-  //TODO
-  // grab all IAM users
-  //compare tags on IAM users to the list
+  val janusUsers: List[String] = getJanusUsernames(dummyJanusData)
+  val credsReport = cacheService.getAllCredentials
+  val iamHumanUsers: List[HumanUser] = getHumanUsers(credsReport)
+  val unrecognisedIamUsers: List[HumanUser] = filterUnrecognisedIamUsers(iamHumanUsers, "name", janusUsers)
+
+  //TODO disable access key and remove password
+  //TODO send notification that this has been done
 }
