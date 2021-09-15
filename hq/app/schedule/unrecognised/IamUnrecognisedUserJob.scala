baaaa960cf769919b14d198eb6d3877a85d06e8d
Jon Soul <jonathan.soul@guardian.co.uk>
2021-09-15 15:36:57 +0100

    Disable keys and login profile in unrecognised job

--- hq/app/schedule/unrecognised/IamUnrecognisedUserJob.scala
+++ hq/app/schedule/unrecognised/IamUnrecognisedUserJob.scala
@@ -1,18 +1,30 @@
 package schedule.unrecognised
 
+import aws.AwsClients
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementAsync
+import com.amazonaws.services.sns.AmazonSNSAsync
+import com.gu.anghammarad.models.{AwsAccount => TargetAccount}
 import com.gu.janus.model.{ACL, AwsAccount, JanusData, SupportACL}
-import model.{CredentialReportDisplay, CronSchedule, HumanUser, AwsAccount => Account}
+import config.Config.getAnghammaradSNSTopicArn
+import model.{CronSchedule, VulnerableUser}
 import org.joda.time.Seconds
-import play.api.Logging
-import schedule.unrecognised.IamUnrecognisedUsers.{filterUnrecognisedIamUsers, getHumanUsers, getJanusUsernames}
+import play.api.{Configuration, Logging}
+import schedule.IamMessages.FormerStaff.disabledUsersMessage
+import schedule.IamMessages.disabledUsersSubject
+import schedule.Notifier.{notification, send}
+import schedule.unrecognised.IamUnrecognisedUsers.{filterUnrecognisedIamUsers, getJanusUsernames}
+import schedule.vulnerable.IamDisableAccessKeys.disableAccessKeys
+import schedule.vulnerable.IamRemovePassword.removePasswords
 import schedule.{CronSchedules, JobRunner}
 import services.CacheService
-import utils.attempt.FailedAttempt
 
-class IamUnrecognisedUserJob(cacheService: CacheService) extends JobRunner with Logging {
+import scala.concurrent.ExecutionContext
+
+class IamUnrecognisedUserJob(cacheService: CacheService, snsClient: AmazonSNSAsync, iamClients: AwsClients[AmazonIdentityManagementAsync], config: Configuration)(implicit val executionContext: ExecutionContext) extends JobRunner with Logging {
   override val id: String = "unrecognised-iam-users"
   override val description: String = "Check for and remove unrecognised human IAM users"
   override val cronSchedule: CronSchedule = CronSchedules.everyWeekDay
+  private val topicArn: Option[String] = getAnghammaradSNSTopicArn(config)
 
   def run(testMode: Boolean): Unit = {
     if (testMode) {
@@ -20,21 +32,40 @@ class IamUnrecognisedUserJob(cacheService: CacheService) extends JobRunner with 
     } else {
       logger.info(s"Running scheduled job: $description")
     }
+
+    val dummyJanusData = JanusData(
+      Set(AwsAccount("Deploy Tools", "deployTools")),
+      ACL(Map("firstName.secondName" -> Set.empty)),
+      ACL(Map.empty),
+      SupportACL(Map.empty, Set.empty, Seconds.ZERO),
+      None
+    )
+
+    val janusUsers: Seq[String] = getJanusUsernames(dummyJanusData)
+    val credsReport = cacheService.getAllCredentials
+
+    credsReport.map { case (account, eitherCredsReportOrFailure) =>
+      eitherCredsReportOrFailure.map { credsReport =>
+        val humanUsers = credsReport.humanUsers
+        val unrecognisedIamUsers: Seq[VulnerableUser] = filterUnrecognisedIamUsers(humanUsers, "name", janusUsers)
+
+        //TODO these should return a value that we can inspect for success/error handling
+        disableAccessKeys(account, unrecognisedIamUsers, iamClients)
+        removePasswords(account, unrecognisedIamUsers, iamClients)
+
+        if(unrecognisedIamUsers.nonEmpty) {
+          val message = notification(
+            disabledUsersSubject(account),
+            disabledUsersMessage(unrecognisedIamUsers),
+            List(TargetAccount(account.accountNumber))
+          )
+          send(
+            message,
+            topicArn,
+            snsClient,
+            testMode
+          )
+        }
+      }
   }
-
-  val dummyJanusData = JanusData(
-    Set(AwsAccount("Deploy Tools", "deployTools")),
-    ACL(Map("firstName.secondName" -> Set.empty)),
-    ACL(Map.empty),
-    SupportACL(Map.empty, Set.empty, Seconds.ZERO),
-    None
-  )
-
-  val janusUsers: List[String] = getJanusUsernames(dummyJanusData)
-  val credsReport = cacheService.getAllCredentials
-  val iamHumanUsers: List[HumanUser] = getHumanUsers(credsReport)
-  val unrecognisedIamUsers: List[HumanUser] = filterUnrecognisedIamUsers(iamHumanUsers, "name", janusUsers)
-
-  //TODO disable access key and remove password
-  //TODO send notification that this has been done
 }
--- hq/app/schedule/unrecognised/IamUnrecognisedUsers.scala
+++ hq/app/schedule/unrecognised/IamUnrecognisedUsers.scala
@@ -1,22 +1,18 @@
 package schedule.unrecognised
 
 import com.gu.janus.model.JanusData
-import model.{AwsAccount, CredentialReportDisplay, HumanUser, Tag}
-import utils.attempt.FailedAttempt
+import model.{HumanUser, VulnerableUser}
 
 object IamUnrecognisedUsers {
-  def getJanusUsernames(janusData: JanusData): List[String] =
+  def getJanusUsernames(janusData: JanusData): Seq[String] =
     janusData.access.userAccess.keys.toList
 
-  def getHumanUsers(credsReport: Map[AwsAccount, Either[FailedAttempt, CredentialReportDisplay]]): List[HumanUser] =
-    credsReport.values.collect { case Right(report) => report.humanUsers }.flatten.toList
-
-  def filterUnrecognisedIamUsers(iamHumanUsersWithTargetTag: List[HumanUser], targetKey: String, janusUsernames: List[String]): List[HumanUser] =
+  def filterUnrecognisedIamUsers(iamHumanUsersWithTargetTag: Seq[HumanUser], targetKey: String, janusUsernames: Seq[String]): Seq[VulnerableUser] =
     iamHumanUsersWithTargetTag.filterNot { iamUser =>
       val maybeTag = iamUser.tags.find(tag => tag.key == targetKey)
       maybeTag match {
         case Some(tag) => janusUsernames.contains(tag.value) // filter out human users that have tags which match the janus usernames
         case None => true
       }
-    }
+    }.map(VulnerableUser.fromIamUser)
 }
