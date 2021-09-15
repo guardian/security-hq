51c75ad73a7ff20694cfdcb045090fac20a047fc
Jon Soul <jonathan.soul@guardian.co.uk>
2021-09-15 10:23:11 +0100

    Resolve test issues

--- hq/test/schedule/IamUsersToDisableTest.scala
+++ hq/test/schedule/IamUsersToDisableTest.scala
@@ -1,40 +1,53 @@
 package schedule
 
 import com.gu.anghammarad.models.Stack
-import model.{AwsAccount, IAMAlertTargetGroup, VulnerableUser}
+import model._
 import org.joda.time.DateTime
 import org.scalatest.{FreeSpec, Matchers}
-import schedule.IamUsersToDisable.toDisableToday
+import schedule.IamUsersToDisable.usersToDisable
 
 class IamUsersToDisableTest extends FreeSpec with Matchers {
 
-  val dynamo: DynamoAlertService = new MockDynamoAlertService()
+  private val dynamo: DynamoAlertService = new MockDynamoAlertService()
 
-  def flaggedUsersWithDeadline(deadline: Option[DateTime]) = Map(
-    AwsAccount("","","","") -> Seq(
+  private val account = AwsAccount("accountId","accountName","accountRole","accountNumber")
+  def flaggedUserWithUsername(username: String) = Map(
+     account -> Seq(
       IAMAlertTargetGroup(
         List(Stack("test-stack")),
         Seq(
           VulnerableUser(
-            username = "username",
+            username = username,
             humanUser = true,
             tags = List.empty,
-            disableDeadline = deadline
+            disableDeadline = None
   )))))
 
   "usersToDisable" - {
     "returns the user when the deadline is today" in {
       val deadline = DateTime.now
       val today = deadline
+      val username = "username1"
 
-      val users = usersToDisable(flaggedUsersWithDeadline(Some(deadline)), dynamo, today)
+      dynamo.putAlert(IamAuditUser(
+        "id",
+        account.name,
+        username,
+        List(IamAuditAlert(
+          VulnerableCredential,
+          today,
+          deadline
+        ))
+      ))
+
+      val users = usersToDisable(flaggedUserWithUsername(username), dynamo, today)
       users.size shouldBe 1
       users.map { case (_, vulnerableUsers) =>
-        vulnerableUsers shouldBe VulnerableUser(
-          username = "username",
+        vulnerableUsers shouldBe List(VulnerableUser(
+          username = username,
           humanUser = true,
           tags = List.empty,
-          disableDeadline = Some(deadline))
+          disableDeadline = None))
       }
     }
 
@@ -42,18 +55,42 @@ class IamUsersToDisableTest extends FreeSpec with Matchers {
       val now = DateTime.now
       val deadline = now.minusDays(1)
       val today = now
+      val username = "username2"
 
-      val users = usersToDisable(flaggedUsersWithDeadline(Some(deadline)), dynamo, today)
-      users shouldBe empty
+      dynamo.putAlert(IamAuditUser(
+        "id",
+        account.name,
+        username,
+        List(IamAuditAlert(
+          VulnerableCredential,
+          today,
+          deadline
+        ))
+      ))
+
+      val users = usersToDisable(flaggedUserWithUsername(username), dynamo, today)
+      users(account) shouldBe empty
     }
 
     "does not return a user when the deadline is after today" in {
       val now = DateTime.now
       val deadline = now.plusDays(1)
       val today = now
+      val username = "username3"
 
-      val users = usersToDisable(flaggedUsersWithDeadline(Some(deadline)), dynamo, today)
-      users shouldBe empty
+      dynamo.putAlert(IamAuditUser(
+        "id",
+        account.name,
+        username,
+        List(IamAuditAlert(
+          VulnerableCredential,
+          today,
+          deadline
+        ))
+      ))
+
+      val users = usersToDisable(flaggedUserWithUsername(username), dynamo, today)
+      users(account) shouldBe empty
     }
   }
 }
