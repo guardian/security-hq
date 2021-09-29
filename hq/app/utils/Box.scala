d9685a362134e6bd0511d469f78146fdd9408fc3
Jacob Winch <jacob.winch@guardian.co.uk>
2021-09-29 17:32:28 +0100

    Make Box's constructor and ref private and prevent overriding ref

--- hq/app/utils/Box.scala
+++ hq/app/utils/Box.scala
@@ -6,8 +6,8 @@ object Box {
   def apply[T](initialValue: T): Box[T] = new Box[T](initialValue)
 }
 
-class Box[T](t: T) {
-  private val ref: AtomicReference[T] = new AtomicReference[T](t)
+class Box[T] private (t: T) {
+  private final val ref: AtomicReference[T] = new AtomicReference[T](t)
   def get(): T = ref.get()
   def send(t: T): Unit = ref.set(t)
 }
