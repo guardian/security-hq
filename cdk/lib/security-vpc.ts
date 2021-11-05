53ba37ef08f5d30d31ec596b06c20c7f2051947e
Nicolas Long <nicolas.long@theguardian.com>
2021-11-05 14:59:43 +0000

    Remove VPC to test https://github.com/guardian/riff-raff/pull/657

--- cdk/lib/security-vpc.ts
+++ cdk/lib/security-vpc.ts
@@ -6,6 +6,6 @@ import { GuVpc } from '@guardian/cdk/lib/constructs/vpc';
 export class SecurityVpc extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
-    new GuVpc(this, 'SecurityVpc', {}); // TODO specify CIDR
+    // new GuVpc(this, 'SecurityVpc', {}); // TODO specify CIDR
   }
 }
