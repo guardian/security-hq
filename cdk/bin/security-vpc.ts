5de60bf10d35b0006b5a0a595a9935e545162312
nic.long <nicolas.long@theguardian.com>
2021-11-19 10:04:46 +0000

    Commit synthed template for VPC stack (#343)
    This is required because the stack needs to be 'environment-aware'
    to generate the right number of subnets and associated resources.
    We don't have a way to provide credentials for this in CI (yet) so
    need to do it manually for now.
    
    See also:
    
    https://docs.aws.amazon.com/cdk/latest/guide/environments.html

---
+++ cdk/bin/security-vpc.ts
@@ -0,0 +1,14 @@
+import 'source-map-support/register';
+import { App } from '@aws-cdk/core';
+import { SecurityVpc } from '../lib/security-vpc';
+
+// Note, this stack is synthed locally rather than in CI as it is
+// 'environment-aware'
+// (https://docs.aws.amazon.com/cdk/latest/guide/environments.html) and so
+// requires AWS crendentials. Ideally, longer-term we'd support this in Github
+// Actions somehow.
+const app = new App();
+new SecurityVpc(app, 'security-vpc', {
+  stack: 'security',
+  env: { region: 'eu-west-1', account: process.env.CDK_DEFAULT_ACCOUNT }, // Crucial to ensure VPC uses all AZs.
+});
