06ae2f3b3f008538443dda3776ba1c204d99df32
nic.long <nicolas.long@theguardian.com>
2021-11-22 10:12:33 +0000

    CDK cleanup (#351)

--- cdk/lib/security-hq.ts
+++ cdk/lib/security-hq.ts
@@ -3,23 +3,23 @@ import {
   Metric,
   TreatMissingData,
 } from '@aws-cdk/aws-cloudwatch';
-import type { CfnTable } from '@aws-cdk/aws-dynamodb';
+import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
 import {
   InstanceClass,
   InstanceSize,
   InstanceType,
   Peer,
 } from '@aws-cdk/aws-ec2';
-import type { CfnTopic } from '@aws-cdk/aws-sns';
-import { CfnInclude } from '@aws-cdk/cloudformation-include';
-import { Duration, Tags } from '@aws-cdk/core';
-import type { App } from '@aws-cdk/core';
+import { EmailSubscription } from '@aws-cdk/aws-sns-subscriptions';
+import { Duration, RemovalPolicy, Tags } from '@aws-cdk/core';
+import type { App, CfnElement } from '@aws-cdk/core';
 import { AccessScope, GuApplicationPorts, GuEc2App } from '@guardian/cdk';
 import { Stage } from '@guardian/cdk/lib/constants/stage';
 import { GuAlarm } from '@guardian/cdk/lib/constructs/cloudwatch';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import {
   GuDistributionBucketParameter,
+  GuParameter,
   GuStack,
 } from '@guardian/cdk/lib/constructs/core';
 import type { AppIdentity } from '@guardian/cdk/lib/constructs/core/identity';
@@ -29,16 +29,8 @@ import {
   GuDynamoDBReadPolicy,
   GuDynamoDBWritePolicy,
 } from '@guardian/cdk/lib/constructs/iam';
+import { GuSnsTopic } from '@guardian/cdk/lib/constructs/sns';
 
-/**
- * Migration steps:
- *
- * - ensure new stack is working okay
- * - make DNS switch (defined in domains account)
- * - include full definitions for adopted resources (dynamo, alarm topic) + stop
- *   importing old template
- * - finally delete old template + stack
- */
 export class SecurityHQ extends GuStack {
   private static app: AppIdentity = {
     app: 'security-hq',
@@ -49,25 +41,9 @@ export class SecurityHQ extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
 
-    // Import old stuff. Note, this template has been modified slightly to avoid
-    // a clash on the VpcId parameter - as the new service runs in a different
-    // VPC to the old.
-    const template = new CfnInclude(this, `security-hq-PROD`, {
-      templateFile: '../cloudformation/security-hq.template.yaml',
-      parameters: {
-        Stage: this.stage,
-      },
-    });
-
-    // Import the existing Dynamo table.
-    const cfnTable = template.getResource(
-      'SecurityHqIamDynamoTable'
-    ) as CfnTable;
-
-    // TODO use below once old template is gone instead of importing above.
-    /*     const table = new Table(this, 'DynamoTable', {
+    const table = new Table(this, 'DynamoTable', {
       tableName: `security-hq-iam-${this.stage}`,
-      removalPolicy: RemovalPolicy.RETAIN, // TODO set update+delete policy too.
+      removalPolicy: RemovalPolicy.RETAIN,
       readCapacity: 5,
       writeCapacity: 5,
       partitionKey: {
@@ -76,17 +52,21 @@ export class SecurityHQ extends GuStack {
       },
     });
     const defaultChild = table.node.defaultChild as unknown as CfnElement;
-    defaultChild.overrideLogicalId('SecurityHqIamDynamoTable'); */
-
-    // The new stack below...
+    defaultChild.overrideLogicalId('SecurityHqIamDynamoTable');
 
     const distBucket = GuDistributionBucketParameter.getInstance(this);
 
     // TODO replace with config repo once token access for GHA is sorted
     // (https://trello.com/c/zhdgXpmk/903-allow-github-actions-to-clone-private-infrastructure-config).
-    const accessRestrictionCidr = template.getParameter(
-      'AccessRestrictionCidr'
-    ).valueAsString;
+    const accessRestrictionCidr = new GuParameter(
+      this,
+      'AccessRestrictionCidr',
+      {
+        description:
+          'CIDR block from which access to Security HQ should be allowed',
+        type: 'String',
+      }
+    );
 
     const domainNames = {
       [Stage.CODE]: { domainName: 'security-hq.code.dev-gutools.co.uk' },
@@ -96,7 +76,7 @@ export class SecurityHQ extends GuStack {
     const ec2App = new GuEc2App(this, {
       access: {
         scope: AccessScope.RESTRICTED,
-        cidrRanges: [Peer.ipv4(accessRestrictionCidr)],
+        cidrRanges: [Peer.ipv4(accessRestrictionCidr.valueAsString)],
       },
       app: 'security-hq',
       applicationPort: GuApplicationPorts.Play,
@@ -119,10 +99,10 @@ dpkg -i /tmp/installer.deb`,
       roleConfiguration: {
         additionalPolicies: [
           new GuDynamoDBReadPolicy(this, 'DynamoRead', {
-            tableName: cfnTable.tableName as string,
+            tableName: table.tableName,
           }),
           new GuDynamoDBWritePolicy(this, 'DynamoWrite', {
-            tableName: cfnTable.tableName as string,
+            tableName: table.tableName,
           }),
           new GuAllowPolicy(this, 'SsmCommands', {
             resources: ['*'],
@@ -157,22 +137,14 @@ dpkg -i /tmp/installer.deb`,
       },
     });
 
-    // Required to support 2 ASGs at the same time in a RR deploy.
-    Tags.of(ec2App.autoScalingGroup).add('gu:riffraff:new-asg', 'true');
-
     new GuCname(this, 'security-hq.gutools.co.uk', {
       app: SecurityHQ.app.app,
       domainNameProps: domainNames,
-      ttl: Duration.minutes(1), // Temporarily low during DNS migration.
+      ttl: Duration.hours(1),
       resourceRecord: ec2App.loadBalancer.loadBalancerDnsName,
     });
 
-    // TODO replace once template deleted with commented code below.
-    const notificationTopic = template.getResource(
-      'NotificationTopic'
-    ) as CfnTopic;
-
-    /*     const notificationTopic = new GuSnsTopic(this, 'NotificationTopic', {
+    const notificationTopic = new GuSnsTopic(this, 'NotificationTopic', {
       displayName: 'Security HQ notifications',
     });
     const emailDest = new GuParameter(this, 'CloudwatchAlarmEmailDestination', {
@@ -180,7 +152,7 @@ dpkg -i /tmp/installer.deb`,
     });
     notificationTopic.addSubscription(
       new EmailSubscription(emailDest.valueAsString)
-    ); */
+    );
 
     new GuAlarm(this, 'RemovePasswordFailureAlarm', {
       app: SecurityHQ.app.app,
@@ -188,7 +160,7 @@ dpkg -i /tmp/installer.deb`,
         'Security HQ failed to remove a vulnerable password (new stack)',
       alarmDescription:
         'The credentials reaper feature of Security HQ logs either success or failure to cloudwatch, and this alarm lets us know when it logs a failure. Check the application logs for more details https://logs.gutools.co.uk/s/devx/goto/f9915a6e4e94a000732d67026cea91be.',
-      snsTopicName: notificationTopic.topicName as string,
+      snsTopicName: notificationTopic.topicName,
       threshold: 1,
       evaluationPeriods: 1,
       metric: new Metric({
@@ -210,7 +182,7 @@ dpkg -i /tmp/installer.deb`,
         'Security HQ failed to disable a vulnerable access key (new stack)',
       alarmDescription:
         'The credentials reaper feature of Security HQ logs either success or failure to cloudwatch, and this alarm lets us know when it logs a failure. Check the application logs for more details https://logs.gutools.co.uk/s/devx/goto/f9915a6e4e94a000732d67026cea91be.',
-      snsTopicName: notificationTopic.topicName as string,
+      snsTopicName: notificationTopic.topicName,
       threshold: 1,
       evaluationPeriods: 1,
       metric: new Metric({
