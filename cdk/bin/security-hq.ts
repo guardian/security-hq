c726cc7377d292e55b69a64cc758bf9d88f6a2d5
Akash Askoolum <akash1810@gmail.com>
2022-05-25 17:14:19 +0100

    refactor: Replace custom scripts with CDK CLI commands

--- cdk/bin/security-hq.ts
+++
@@ -1,6 +0,0 @@
-import 'source-map-support/register';
-import { App } from 'aws-cdk-lib';
-import { SecurityHQ } from '../lib/security-hq';
-
-const app = new App();
-new SecurityHQ(app, 'security-hq', { stack: 'security', stage: 'PROD' });
