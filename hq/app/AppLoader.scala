166ec84f251c7a9df514da4fe2aaeb983d5ea50b
akash1810 <akash1810@users.noreply.github.com>
2023-11-07 06:52:23 +0000

    chore(deps): Remove `com.gu.kinesis-logback-appender`
    Since adopting GuCDK's application log shipping capability,
    this dependency is no longer needed.

--- hq/app/AppLoader.scala
+++ hq/app/AppLoader.scala
@@ -1,5 +1,3 @@
-import config.LoggingConfig
-import logging.LogConfig
 import play.api.ApplicationLoader.Context
 import play.api.{Application, ApplicationLoader, Mode}
 
@@ -8,15 +6,6 @@ import scala.concurrent.Future
 
 class AppLoader extends ApplicationLoader {
   override def load(context: Context): Application = {
-    val loggingConfig = LoggingConfig(
-      context.initialConfiguration,
-      sys.env.getOrElse("LOCAL_LOG_SHIPPING", "false").toBoolean
-    )
-
-    LogConfig.initPlayLogging(context)
-    LogConfig.initRemoteLogShipping(loggingConfig)
-    LogConfig.initLocalLogShipping(loggingConfig)
-
     val components = new AppComponents(context)
 
     components.application
