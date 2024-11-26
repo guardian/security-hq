f73cf135efe82cb88b5d430ebcc4857ffeb99235
Mariot Chauvin <mariot.chauvin@gmail.com>
2024-11-26 14:50:32 +0000

    Add types to implicits to ensure compilation works.

--- lambda/common/src/main/scala/com/gu/hq/lambda/model/JSON.scala
+++ lambda/common/src/main/scala/com/gu/hq/lambda/model/JSON.scala
@@ -8,21 +8,21 @@ import play.api.libs.json.{Json, Reads}
 //noinspection TypeAnnotation
 object JSON {
   private val dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-  implicit val jodaDateReads = Reads[DateTime](js =>
+  implicit val jodaDateReads: Reads[DateTime] = Reads[DateTime](js =>
     js.validate[String].map[DateTime](dtString =>
       DateTime.parse(dtString, DateTimeFormat.forPattern(dateFormat)).withZone(DateTimeZone.UTC)
     )
   )
 
-  implicit val tagRead = Json.reads[Tag]
+  implicit val tagRead: Reads[Tag] = Json.reads[Tag]
 
-  implicit val relationshipRead = Json.reads[Relationship]
-  implicit val configurationItemRead = Json.reads[ConfigurationItem]
-  implicit val configurationItemDiffRead = Json.reads[ConfigurationItemDiff]
-  implicit val invokingEventRead = Json.reads[InvokingEvent]
+  implicit val relationshipRead: Reads[Relationship] = Json.reads[Relationship]
+  implicit val configurationItemRead: Reads[ConfigurationItem] = Json.reads[ConfigurationItem]
+  implicit val configurationItemDiffRead: Reads[ConfigurationItemDiff] = Json.reads[ConfigurationItemDiff]
+  implicit val invokingEventRead: Reads[InvokingEvent] = Json.reads[InvokingEvent]
 
-  implicit val userIdGroupPairRead = Json.reads[UserIdGroupPair]
-  implicit val ipPermissionRead = Json.reads[IpPermission]
-  implicit val sgConfigurationRead = Json.reads[SGConfiguration]
-  implicit val accountsRead = Json.reads[AccountMap]
+  implicit val userIdGroupPairRead: Reads[UserIdGroupPair] = Json.reads[UserIdGroupPair]
+  implicit val ipPermissionRead: Reads[IpPermission] = Json.reads[IpPermission]
+  implicit val sgConfigurationRead: Reads[SGConfiguration] = Json.reads[SGConfiguration]
+  implicit val accountsRead: Reads[AccountMap] = Json.reads[AccountMap]
 }
