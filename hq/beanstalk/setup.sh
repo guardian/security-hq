5da0f7a8f0a67df3dad83ff02bca3a634aad66a2
Justin Rowles <justin.rowles.contractor@guardian.co.uk>
2026-08-11 15:35:11 +0000

    Remove SHQ app code

--- hq/beanstalk/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# on-instance setup, fetches application configuration
-
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-touch /tmp/test-setup-ran
-
-aws s3 cp s3://$HQ_CONFIG_BUCKET/$HQ_CONFIG_PATH $ROOT
-aws s3 cp s3://$HQ_CONFIG_BUCKET/$HQ_CERT_PATH $ROOT
